2073325fb64a748410476e5237ded57c431a7250
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-12-02 20:35:34 +0000

    Add intro text

--- src/js/manytwitch.js
+++ src/js/manytwitch.js
@@ -174,23 +174,33 @@ Vue.component('streams', {
       this.$emit('update-streams', this.orderedStreams)
     }
   },
-  template: `<draggable 
-              class="streams"
-              v-model="orderedStreams" 
-              @start="drag=true" 
-              @end="drag=false"
-              :options="{ghostClass:'ghost', handle:'.handle', filter:'.stream-player', forceFallback:'true', fallbackTolerance:'2'}">
-                <stream 
-                  class="draggable" 
-                  v-for="stream in orderedStreams"
-                  :key="stream.embedPlayerID"
-                  :stream="stream"
-                  :numStreams="streams.length"
-                  :options="options"
-                  :hover="drag"
-                  v-on:remove-stream="removeStream"
-                  ></stream>
-              </draggable>`,
+  template: `<div class="streams-container">
+              <draggable 
+                v-if="streams.length"
+                class="streams"
+                v-model="orderedStreams" 
+                @start="drag=true" 
+                @end="drag=false"
+                :options="{ghostClass:'ghost', handle:'.handle', filter:'.stream-player', forceFallback:'true', fallbackTolerance:'2'}">
+                  <stream 
+                    class="draggable" 
+                    v-for="stream in orderedStreams"
+                    :key="stream.embedPlayerID"
+                    :stream="stream"
+                    :numStreams="streams.length"
+                    :options="options"
+                    :hover="drag"
+                    v-on:remove-stream="removeStream"
+                    ></stream>
+                </draggable>
+                <section 
+                  v-if="!streams.length"
+                  class="intro-content">
+                    <h1 class="intro-title">MANYTWITCH</h1>
+                    <h2 class="intro-subheading">Multiple Twitch Stream Viewer</h2>
+                    <p class="intro-text">Enter a twitch channel to start.</p>
+                </section>
+              </div>`,
   methods: {
     removeStream: function (removedStream) {
       // Remove stream with matching streamIndex from currentStreams
