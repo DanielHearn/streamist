f9a20d82f8bfdecd30b0eb522512a6a7f8315c30
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-03-25 20:55:02 +0000

    Fix refresh and remove chat buttons

--- src/js/components/chat/chatItem/ChatItem.vue
+++ src/js/components/chat/chatItem/ChatItem.vue
@@ -5,12 +5,12 @@
       <option v-for="stream in filteredStreams" :key="stream.embedPlayerID">{{ stream.streamName }}</option>
     </select>
     <div class="chat-controls">
-      <icon-button :iconName="$options.icons.refresh" title="Remove Chat" @click.native="remove"/>
+      <icon-button :iconName="$options.icons.refresh" title="Refresh Chat" @click.native="refresh"/>
       <icon-button
         v-if="removeAvailable"
         :iconName="$options.icons.remove"
-        title="Refresh Chat"
-        @click.native="refresh"
+        title="Remove Chat"
+        @click.native="remove"
       />
     </div>
     <iframe frameborder="0" scrolling="no" v-if="chatVisible" :id="id" :src="src"></iframe>
