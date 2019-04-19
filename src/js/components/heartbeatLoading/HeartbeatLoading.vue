b19c33154ffca9e727a0bd5ddbbf804d955b4e17
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-04-19 20:02:57 +0100

    Add heartbeat loading component

---
+++ src/js/components/heartbeatLoading/HeartbeatLoading.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="heartbeatLoading" :class="{hidden: !active}">
+    <div class="pulse"></div>
+    <div class="pulse"></div>
+  </div>
+</template>
+
+<script src="./heartbeatLoading.js"></script>
+
+<style lang="scss">
+@import "./heartbeatLoading.scss";
+</style>
---
+++ src/js/components/heartbeatLoading/heartbeatLoading.js
@@ -0,0 +1,10 @@
+export default {
+  name: 'heartbeat-loading',
+  props: {
+    active: {
+      type: Boolean,
+      default: true,
+      required: true
+    }
+  }
+}
