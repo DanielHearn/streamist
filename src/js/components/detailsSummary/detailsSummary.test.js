db5ebc889378855b6055ab9d82900909f1819f39
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-07-19 12:41:06 +0100

    Add details summary tests

---
+++ src/js/components/detailsSummary/detailsSummary.test.js
@@ -0,0 +1,51 @@
+import { mount } from '@vue/test-utils'
+import DetailsSummary from './DetailsSummary.vue'
+
+describe('Details Summary', () => {
+  test('Toggles once clicked', () => {
+    const wrapper = mount(DetailsSummary)
+
+    const summary = wrapper.find('summary')
+    summary.trigger('click')
+
+    // Check active
+    expect(wrapper.html()).toContain('<details class="details-summary active">')
+
+    summary.trigger('click')
+    // Check closed
+    expect(wrapper.html()).toContain('<details class="details-summary">')
+  })
+
+  test('Slots', () => {
+    const wrapper = mount(DetailsSummary, {
+      scopedSlots: {
+        header: '<p>Header Content</p>',
+        content: '<p>Main Content</p>'
+      }
+    })
+
+    const summary = wrapper.find('summary')
+    const content = wrapper.find('.details-summary__content')
+
+    expect(summary.html()).toContain(
+      '<summary class="details-summary__header text-sub-heading"><p>Header Content</p></summary>'
+    )
+    expect(content.html()).toContain(
+      '<div class="details-summary__content"><p>Main Content</p></div>'
+    )
+  })
+
+  test('Header classes', () => {
+    const wrapper = mount(DetailsSummary, {
+      propsData: {
+        headerClasses: 'custom-header-class'
+      }
+    })
+
+    const summary = wrapper.find('summary')
+
+    expect(summary.html()).toContain(
+      '<summary class="details-summary__header text-sub-heading custom-header-class"></summary>'
+    )
+  })
+})
