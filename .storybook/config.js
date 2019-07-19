9ed3f289d41010e3e225cccb6db7526d532f27ae
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-07-19 12:40:58 +0100

    Add jest test config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,18 @@
 import { configure } from '@storybook/vue'
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/js/components', true, /\.stories\.js$/)
+// const req = require.context('../src/js/components', true, /\.stories\.js$/)
+
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
+registerRequireContextHook()
+
+const req = global.__requireContext(
+  __dirname,
+  '../src/js/components',
+  true,
+  /\.stories\.js$/
+)
+
 function loadStories () {
   req.keys().forEach(filename => req(filename))
 }
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  setupFiles: ['./setup-jest.js']
+  setupFiles: ['./setup-jest.js'],
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest'
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!(@storybook/.*\\.vue$))'
+  ]
 }
