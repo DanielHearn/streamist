1db240e4ec98f5ec6c0ae90224063d2907c3c3db
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-07-20 18:04:20 +0100

    Add menuItem tests

---
+++ src/js/components/menu/menuItem/menuItem.test.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils'
+import MenuItem from './MenuItem.vue'
+
+describe('MenuItem', () => {
+  const title = '<p>Menu Title</p>'
+  const actionsContent = '<p>Actions Content</p>'
+  const mainContent = '<p>Main Content</p>'
+
+  const wrapper = mount(MenuItem, {
+    propsData: {
+      title: 'Menu Title',
+      closeTitle: 'Close title'
+    },
+    slots: {
+      actions: actionsContent,
+      content: mainContent
+    }
+  })
+
+  const actions = wrapper.find('.menu-item-actions')
+  const content = wrapper.find('.menu-item-content')
+
+  test('Title', () => {
+    const titleText = wrapper.find('.text-heading').text()
+
+    expect(titleText).toBe(title)
+  })
+
+  test('Slots', () => {
+    expect(actions.html()).toContain(actionsContent)
+    expect(content.html()).toContain(mainContent)
+  })
+})
