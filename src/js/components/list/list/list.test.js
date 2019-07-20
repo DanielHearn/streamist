fad36dbc74a3e0dde753e420a7364926e870d8db
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-07-20 14:23:07 +0100

    Add list tests

---
+++ src/js/components/list/list/list.test.js
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils'
+import List from './List.vue'
+
+describe('List', () => {
+  test('Default layout', () => {
+    const wrapper = mount(List)
+    expect(wrapper.attributes('class')).toContain('list--layout-column')
+  })
+
+  test('layoutClass', () => {
+    const wrapper = mount(List, {
+      propsData: {
+        layout: 'row'
+      }
+    })
+    expect(wrapper.attributes('class')).toContain('list--layout-row')
+  })
+
+  test('Slots', () => {
+    const content = '<p>Content</p>'
+    const wrapper = mount(List, {
+      scopedSlots: {
+        default: content
+      }
+    })
+    expect(wrapper.html()).toContain(content)
+  })
+})
