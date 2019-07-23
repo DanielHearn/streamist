28ff99aeb2a1b8153e5dc6368c42708c9188123e
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-07-23 09:53:29 +0100

    Add controlbar tests

---
+++ src/js/components/controlBar/controlBar.test.js
@@ -0,0 +1,15 @@
+import { mount } from '@vue/test-utils'
+import ControlBar from './ControlBar.vue'
+
+describe('DetailsSummary', () => {
+  test('Slots', () => {
+    const content = '<p>Main Content</p>'
+    const wrapper = mount(ControlBar, {
+      slots: {
+        default: content
+      }
+    })
+
+    expect(wrapper.html()).toContain(content)
+  })
+})
