ef5d370566b523000266eb6759e1463d47dbb568
Daniel Hearn <DanielHearn@users.noreply.github.com>
2019-07-23 10:17:19 +0100

    Add iconButton tests

---
+++ src/js/components/inputs/buttons/iconButton/iconButton.test.js
@@ -0,0 +1,72 @@
+import { mount } from '@vue/test-utils'
+import IconButton from './IconButton.vue'
+import Icon from './../../../../Icons'
+
+describe('IconButton', () => {
+  test('Slots', () => {
+    const content = '<p>Main Content</p>'
+    const wrapper = mount(IconButton, {
+      slots: {
+        default: content
+      }
+    })
+
+    expect(wrapper.html()).toContain(content)
+  })
+
+  test('disabled', () => {
+    const wrapper = mount(IconButton)
+
+    expect(wrapper.attributes('disabled')).toBe(undefined)
+    wrapper.setProps({ disabled: true })
+
+    expect(wrapper.attributes('disabled')).toBe('disabled')
+  })
+
+  test('buttonClasses', () => {
+    const wrapper = mount(IconButton)
+
+    expect(wrapper.attributes('class')).toContain('button--accent')
+    wrapper.setProps({ buttonClasses: 'button--different' })
+
+    expect(wrapper.attributes('class')).toContain('button--different')
+  })
+
+  test('iconName', () => {
+    const wrapper = mount(IconButton)
+
+    expect(wrapper.find('i').text()).toBe('mouse')
+    wrapper.setProps({ iconName: Icon.play })
+
+    expect(wrapper.find('i').text()).toBe(Icon.play)
+  })
+
+  test('iconClasses', () => {
+    const wrapper = mount(IconButton)
+
+    expect(wrapper.find('i').attributes('class')).toBe('material-icons')
+    wrapper.setProps({ iconClasses: 'icon-class' })
+
+    expect(wrapper.find('i').attributes('class')).toBe(
+      'material-icons icon-class'
+    )
+  })
+
+  test('hasWarningColor', () => {
+    const wrapper = mount(IconButton)
+
+    expect(wrapper.vm.warningClass).toBe('')
+    wrapper.setProps({ hasWarningColor: true })
+
+    expect(wrapper.vm.warningClass).toBe('button--warning')
+  })
+
+  test('warningClass', () => {
+    const wrapper = mount(IconButton)
+
+    expect(wrapper.attributes('class')).not.toContain('button--warningt')
+    wrapper.setProps({ hasWarningColor: true })
+
+    expect(wrapper.attributes('class')).toContain('button--warning')
+  })
+})
